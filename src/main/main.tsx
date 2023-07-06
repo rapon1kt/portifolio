fda2563fe777517ffb62c1a137481ecae929e249
rapon1kt <raponikt@gmail.com>
2023-07-06 11:41:40 -0300

    refactor: changing import of portfolio.tsx

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Portifolio from "../presentation/pages/portfolio.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<Portifolio />
+	</React.StrictMode>
+);
