bec0a478d088b09a8d056f7e98a6702beff3b861
rapon1kt <raponikt@gmail.com>
2023-07-08 23:50:15 -0300

    feat: create data.ts of skills component

---
+++ src/data/data.ts
@@ -0,0 +1,19 @@
+const data = [
+	{
+		name: "Javascript",
+		experience: "2 year",
+		img: "assets/javascript.png",
+	},
+	{
+		name: "React",
+		experience: "2 year",
+		img: "assets/react.png",
+	},
+	{
+		name: "Typescript",
+		experience: "1 year",
+		img: "assets/typescript.png",
+	},
+];
+
+export default data;
