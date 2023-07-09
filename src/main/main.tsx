06a41ba5d13e2117ba8c8d7e2cca6a523dfe0913
rapon1kt <raponikt@gmail.com>
2023-07-08 23:46:54 -0300

    refactor: fix import of portfolio.tsx

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portifolio from "../presentation/pages/portfolio.tsx";
+import Portifolio from "../presentation/pages/portfolio/portfolio.tsx";
 import "./styles/global-styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
