147fd60075994dcd764d6ac5b1b1639469174cbb
rapon1kt <raponikt@gmail.com>
2023-07-08 23:49:58 -0300

    feat: add the new imports of components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Navbar } from "./navbar/navbar";
+export { default as Skills } from "./skills/skills";
+export { default as About } from "./about/about";
+export { default as Divisor } from "./divisor/divisor";
