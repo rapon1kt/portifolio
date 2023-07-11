ebf41aad206ca58e6d8e36d699ca04f858cfecce
rapon1kt <raponikt@gmail.com>
2023-07-11 20:09:47 -0300

    feat: add new responsives css's

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -8,28 +8,21 @@ import "./navbar-styles.css";
 const Navbar: React.FC = () => {
 	return (
 		<nav className="navbar">
-			<AllInclusiveOutlinedIcon
-				sx={{ fontSize: "4rem", color: "#8f8f8f", width: "20%" }}
-			/>
-			<h2 className="navbar-phrase">
-				"The learned man is a discoverer of facts that already exist - but the
-				wise man is a creator of values ​​that do not exist and which he makes
-				exist." - Albert Einstein
-			</h2>
+			<AllInclusiveOutlinedIcon sx={{ fontSize: "4rem", color: "#8f8f8f" }} />
 			<ul className="navbar-medias">
 				<li className="nav-item">
 					<a className="nav-link" href="https://github.com/rapon1kt">
-						<GitHubIcon sx={{ fontSize: "2rem" }} />
+						<GitHubIcon sx={{ fontSize: "2.4rem" }} />
 					</a>
 				</li>
 				<li className="nav-item">
 					<a className="nav-link" href="https://www.linkedin.com/in/raponikt/">
-						<LinkedInIcon sx={{ fontSize: "2.4rem" }} />
+						<LinkedInIcon sx={{ fontSize: "2.8rem" }} />
 					</a>
 				</li>
 				<li className="nav-item">
 					<a className="nav-link" href="https://www.instagram.com/raponi_13">
-						<InstagramIcon sx={{ fontSize: "2.4rem" }} />
+						<InstagramIcon sx={{ fontSize: "2.8rem" }} />
 					</a>
 				</li>
 			</ul>
