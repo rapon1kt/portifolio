a9b7e0fe9217ef6986d82b940e35bffb50194bca
rapon1kt <raponikt@gmail.com>
2023-07-11 19:57:42 -0300

    feat: create footer component

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer-styles.css";
+
+const Footer: React.FC = () => {
+	return (
+		<div className="footer-container">
+			<h3 className="footer-text">Made with 🖤 by rapon1kt </h3>
+		</div>
+	);
+};
+
+export default Footer;
