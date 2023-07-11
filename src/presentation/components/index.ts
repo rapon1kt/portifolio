40cb006070b9e6c8fa4d461e595c95abda2536a3
rapon1kt <raponikt@gmail.com>
2023-07-11 20:08:59 -0300

    feat: export footer component from components fold

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./navbar/navbar";
 export { default as Skills } from "./skills/skills";
 export { default as About } from "./about/about";
 export { default as Divisor } from "./divisor/divisor";
+export { default as Footer } from "./footer/footer";
